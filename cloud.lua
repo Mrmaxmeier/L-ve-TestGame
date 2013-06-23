3a032b56ceb79029b3d20c38cce848421136ab54
felix <felixbauckholt AT gmail PUNKT com>
2013-06-23 14:13:42 +0200

    TUNNEL!!!!!!!!!!!!!!!

--- cloud.lua
+++ cloud.lua
@@ -3,24 +3,25 @@ require "class"
 cloud = class:new()
 
 function cloud:init()
-	z = 1/(math.random()+0.1)
+	z = 1/(math.random()+0.05)*0.7
 	self.dx = 40*z
 	self.dy = 20*z
-	self.x = math.random(-SIZEX, SIZEX)
-	self.y = (SIZEY/2-self.dy) - 50*z
-	self.speed = z * 60
+	self.rot = math.random(360)
+	self.y = 50*z
+	self.speed = math.random(10,50)
 end
 
 function cloud:update(dt)
-	self.x = self.x + self.speed*dt
-	if self.x > SIZEX then
-		self.x = self.x - 2*SIZEX
-	end
+	self.rot = self.rot + self.speed*dt
 end
 
 function cloud:draw()
 	love.graphics.setColor(255, 255, 255, 125)
-	x, y = self.x, self.y
-	x2, y2 = x+self.dx,y+self.dy
+	love.graphics.push()
+	love.graphics.translate(SIZEX/2, SIZEY/2)
+	love.graphics.rotate(math.rad(self.rot))
+	x, y = -self.dx/2, self.y
+	x2, y2 = self.dx/2, y+self.dy
 	love.graphics.polygon("fill", x, y, x, y2, x2, y2, x2, y)
+	love.graphics.pop()
 end
