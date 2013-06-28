35fb3b18a108d8faa5b47107af88e5d936992c0d
felix <felixbauckholt AT gmail PUNKT com>
2013-06-28 22:03:49 +0200

    player ist eine Klasse und TOOOOOODe werden erkannt

--- player.lua
+++ player.lua
@@ -1,17 +1,23 @@
-function playerInit()
-	
-	player = {
-		x = SIZEX/2,
-		y = SIZEY/2,
-		speed = 400
-	}
-	
-	return player
+require "class"
+
+player = class:new()
+
+function player:init()
+	self.x = SIZEX/2
+	self.y = SIZEY/2
+	self.speed = 400
+	self.r = 64
 end
 
 
-function playerUpdate()
-	
-	
-	
+function player:update(dt)
+	if love.keyboard.isDown("up") then self.y = self.y - self.speed*dt end
+	if love.keyboard.isDown("down") then self.y = self.y + self.speed*dt end
+	if love.keyboard.isDown("left") then self.x = self.x - self.speed*dt end
+	if love.keyboard.isDown("right") then self.x = self.x + self.speed*dt end
+end
+
+function player:draw()
+	love.graphics.setColor(255, 255, 255, 255)
+	lg.draw(hamster, self.x, self.y, math.rad(rot), 1, 1, self.r, self.r)
 end
